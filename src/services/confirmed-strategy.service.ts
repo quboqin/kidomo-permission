44cd6fc2f9bdbd003509fdc57dd279cf75b60e56
Qubo Qin <quboqin0710@gmail.com>
2024-07-04 13:48:15 +0800

    remove permission

--- src/services/confirmed-strategy.service.ts
+++ src/services/confirmed-strategy.service.ts
@@ -4,10 +4,10 @@ import { IUser } from '../interfaces/user.interface'
 export class ConfirmedStrategyService implements IPermissionStrategy {
   public getAllowedPermissions(user: IUser, permissions: string[]): string[] {
     const forbiddenPermissions = [
-      'task_search_by_user_id',
-      'task_create',
-      'task_delete_by_id',
-      'task_update_by_id'
+      // 'task_search_by_user_id',
+      // 'task_create',
+      // 'task_delete_by_id',
+      // 'task_update_by_id'
     ]
     return user.is_confirmed
       ? permissions
